70ff91ffbb6bdf29367610b7fc70b0c525e0172b
marcelhannouti <37220618+marcelhannouti@users.noreply.github.com>
2022-09-02 21:18:47 +0200

    Add files via upload

---
+++ EjerciciosJava/src/Ejercicio8.java
@@ -0,0 +1,15 @@
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+	int num1 = (int) Math.pow(2, 8);
+	System.out.println(" rango  : "+ num1/2);
+	int num2 = (int) Math.pow(2, 16);
+	System.out.println(" rango : "+ num2/2);
+	long num3 = (long) Math.pow(2, 32);
+	System.out.println("rango: "+ num3/2);
+	long num4 = (long) Math.pow(2, 64);
+	System.out.println("rango: "+ num4/2);
+	}
+
+}
